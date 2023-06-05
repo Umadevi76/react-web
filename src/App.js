c0b70ecfd75d62705a5f9d382e197584d371e436
Umadevi76 <umadevir99@gmail.com>
2023-06-06 00:44:32 +0530

    Second Commit

--- src/App.js
+++ src/App.js
@@ -5,41 +5,58 @@ import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import './App.css'
 import About from './components/About';
+import Alert from './components/Alert';
 import {
-  BrowserRouter,
+  BrowserRouter as Router,
   Routes,
   Route,
   Link
 } from "react-router-dom";
 
+
+
 function App() {
   const [mode,setMode]=useState("light");
+
+  const [alert,setAlert]= useState(null);
+
+  const showAlert=(message,type) =>{
+    setAlert({
+      msg: message,
+      type: type
+    })
+   setTimeout(()=>{
+     setAlert(null);
+   },1500)
+  }
+
   const toggleMode = ()=>{
     if(mode==='light'){
     setMode("dark");
     document.body.style.backgroundColor='#042743';
+    showAlert("Dark mode has been enabled","success");
   }
   else{
     setMode("light");
     document.body.style.backgroundColor='white';
+    showAlert("Light mode has been enabled","success");
   }
 }
   return (
-    <div>
-      <BrowserRouter>
+    <>
+    <Router>
      <Navbar title="TextUtils" page="Home" mode={mode} toggleMode={toggleMode}/>
-     <Routes>
-          <Route path="/about" element={<About />}>
-          </Route>
-          
-          <Route path="/" element={<div className='container'>
-              <TextForm heading="Enter the text to analyse" mode={mode}/>
-            </div>}>
-          </Route>
-        </Routes>
+      <Alert alert={alert} />
+      <div className='container'>
+      <Routes>   
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/" element={<TextForm heading="Enter the text to analyse" showAlert={showAlert} mode={mode}/>} />
+      </Routes>
+      </div>
+         
     
-        </BrowserRouter>
-    </div>
+    </Router>
+    </>
   )
 }
 
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -6,14 +6,17 @@ function TextForm(props) {
   const handleUpClick = () =>{
     let newText= text.toUpperCase();
     setText(newText);
+    props.showAlert("Converted to Uppercase!","success"); 
   }
   const handleLoClick = () =>{
     let newText= text.toLowerCase();
-    setText(newText);
+    setText(newText); 
+    props.showAlert("Converted to Lowercase!","success"); 
   }
   const handleClearText = () =>{
     let newText = " ";
     setText(newText);
+    props.showAlert("Text Cleared!","success"); 
   }
   const handleOnChange = (event) =>{
     setText(event.target.value);
